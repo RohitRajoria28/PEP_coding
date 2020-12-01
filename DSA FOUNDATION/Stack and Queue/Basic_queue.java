d79a297015ff0642a428ab3d2a5367494432c8aa
RohitRajoria28 <55023373+RohitRajoria28@users.noreply.github.com>
2020-12-01 14:47:21 +0530

    Add files via upload

---
+++ DSA FOUNDATION/Stack and Queue/Basic_queue.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stack < Integer > st = new Stack < Integer >() ;
+
+        System.out.println(st + "---->" + st.size());
+        st.push(10);
+        System.out.println(st + "---->" + st.size());
+        st.push(20);
+        System.out.println(st + "---->" + st.size());
+        st.push(30);
+        System.out.println(st + "---->" + st.size());
+         System.out.println(st.peek());
+         System.out.println(st.pop());
+         System.out.println(st.peek());
+
+    }
+}
